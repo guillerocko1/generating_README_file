24dea3d5af31885842b9e2cbad441271ea7ac15e
Guillermo Hernandez <60200844+guillerocko1@users.noreply.github.com>
2022-10-20 17:58:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,218 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+//console.log(index_page.coding)
+inquirer
+   .prompt([
+      {
+         type: 'input',
+         message: 'What is the name of the Project? ',
+         name: 'title',
+      },
+      {
+         type: 'input',
+         message: 'Enter a Description of the Project ',
+         name: 'description',
+      },
+      {
+         type: 'input',
+         message: 'Describe the motivation of the project ',
+         name: 'content',
+      },
+      {
+         type: 'input',
+         message: 'What are the Installation Steps? (Separate steps by comma) ',
+         name: 'installation_steps',
+      },
+      {
+         type: 'input',
+         message: 'How is this software mean to be used? ',
+         name: 'usage',
+      },
+      {
+         type: 'list',
+         message: 'Seleect your license ',
+         name: 'license',
+         choices: ["MIT", "GPL", "BSD", "Apache"],
+      },
+      {
+         type: 'input',
+         message: 'Who are contributing in the project? ',
+         name: 'contributers',
+      },
+      {
+         type: 'input',
+         message: 'What is your Github username? ',
+         name: 'username',
+      },
+      {
+         type: 'input',
+         message: 'What is your email address? ',
+         name: 'email',
+      },
+   ])
+   .then((answers) => {
+      /////////////
+      console.log(answers.installation_steps);
+      const html_page = `# ${answers.title}
+${badges(answers.license)}
+     
+<div id="badges" align="center">
+  <img src="https://komarev.com/ghpvc/?username=guillerocko1&style=flat-square&color=blue" alt=""/>
+ </div>
+
+<h1 align="center">
+  hey there
+  <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="30px"/>
+</h1>
+
+<div align="center">
+  <img src="https://media.giphy.com/media/dWesBcTLavkZuG35MI/giphy.gif" width="600" height="300"/>
+</div>
+      
+---
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
+      
+---       
+## Description
+${answers.description}
+
+- What was your motivation?
+This project is an amazing opportunity to develop a deep knowdledge about Nodejs, how to use inquirer and require package, beside that, this project allows to the students to put in practice in real time project, all this tools together in order to get genearate a README file.
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+I built this project because it will allow me to learn to generate a README file. The README file is very important for any project on Github, because it's the best way to let the users to understand our projects with all the important details.
+- What problem does it solve?
+The problems I faced meanwhile I was developing this project was to understand the way require and inquirer work. Also, It was a little difficult for me to find the best way to generate a README file because I was getting some extra characters in the generated file and it was difficult to read it.
+- What did you learn?
+I learned to combine require and inquirer. How important and useful is inquirer package in order to get information from the user and also, how useful is the require package in order to read and create files.
+
+---    
+## Installation
+---
+# Installation
+${answers.installation_steps}
+                  
+The steps requiered to run this application are the followings:
+- Make sure you are in the index.js directory.
+- Open the terminal 
+- Type node index
+- Answer all the information that will fill out the README file .
+- Once the information is completed, the README file will be generated.
+            
+            
+---          
+## Usage
+      
+# ${answers.usage}
+      
+Provide instructions and examples for use. Include screenshots as needed.
+            
+To add a screenshot, create an (image directory) folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+                           
+            
+## Credits
+            
+List your collaborators, if any, with links to their GitHub profiles.
+            
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+            
+If you followed tutorials, include links to those here as well.
+            
+Links used to develop this application:
+- https://www.npmjs.com/package/inquirer/v/8.2.4
+- https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+- https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/about-writing-and-formatting-on-github#enabling-fixed-width-fonts-in-the-editor
+-https://nodejs.org/api/fs.html#fswritefd-string-position-encoding-callback
+      
+---     
+## License
+      
+# ${answers.license}
+
+---             
+## MIT License
+The MIT License is a permissive free software license originating at the Massachusetts Institute of Technology (MIT)[6] in the late 1980s.[7] As a permissive license, it puts only very limited restriction on reuse and has, therefore, high license compatibility
+
+---
+## GPL License
+The GNU General Public License (GNU GPL or simply GPL) is a series of widely used free software licenses that guarantee end users the four freedoms to run, study, share, and modify the software.[7] The license was the first copyleft for general use and was originally written by the founder of the Free Software Foundation (FSF), Richard Stallman, for the GNU Project. 
+---
+## BSD License
+BSD licenses are a family of permissive free software licenses, imposing minimal restrictions on the use and distribution of covered software. This is in contrast to copyleft licenses, which have share-alike requirements. The original BSD license was used for its namesake, the Berkeley Software Distribution (BSD), a Unix-like operating system. The original version has since been revised, and its descendants are referred to as modified BSD licenses.
+ 
+---Apache License
+The Apache License is a permissive free software license written by the Apache Software Foundation (ASF).It allows users to use the software for any purpose, to distribute it, to modify it, and to distribute modified versions of the software under the terms of the license, without concern for royalties. The ASF and its projects release their software products under the Apache License. The license is also used by many non-ASF projects.
+
+            
+---          
+## Contributing
+      
+This is the list of people who have made this project possible thrugh theur contributions.
+# ${answers.contributers}
+
+
+### :fire: My Stats :
+
+[![GitHub Streak](http://github-readme-streak-stats.herokuapp.com?user=${answers.username}&theme=dark&background=000000)](https://git.io/streak-stats)
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=${answers.username}&layout=compact&theme=vision-friendly-dark)](https://github.com/anuraghazra/github-readme-stats)
+            
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+            
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+            
+## Features
+            
+If your project has a lot of features, list them here.
+            
+## How to Contribute
+            
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+            
+
+---
+## Questions
+
+## Github username: ${answers.username}
+## Email address: <a href = "mailto: ${answers.email}">${answers.email}</a>`;
+      
+      //////////// 
+      fs.writeFile('README.md', html_page, 'ascii',
+         // callback function that is called after writing file is done
+         function (err) {
+            if (err) throw err;
+            // if no error
+            console.log("Data is written to file successfully.")
+         });
+   });  
+   
+// function CreateInstallationSteps(steps) {
+//    const array = steps.split(", ")
+
+//    array.foreach(step => {
+//       return `- ${step}`;
+//    })
+//    }
+
+function badges(license) {
+   if (license === "MIT") {
+      return "![MIT](https://img.shields.io/badge/license-MIT-blue)"
+   }
+   else if (license === "GPL") {
+      return "![GPL](https://img.shields.io/badge/license-GPL-brightgreen)"
+   }
+   else if (license === "BSD") {
+      return "![BSD](https://img.shields.io/badge/license-BSD-red)"
+   }
+   
+   else if (license === "APACHE") {
+      return "![APACHE](https://img.shields.io/badge/license-APACHE-yellowgreen)"
+   }
+}
